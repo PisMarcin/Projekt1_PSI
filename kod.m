ec193a2dc10ac0990a7be263f0ba700c10db931e
Marcin Pis <43877871+PisMarcin@users.noreply.github.com>
2018-10-25 20:48:16 +0200

    Update kod.m

--- kod.m
+++ kod.m
@@ -1,20 +1,22 @@
 close all; clear all; clc;
 
-net=newp([0 1; -2 2],1); %tworzy perceptron sk�adajacy sie z 2 elementow
-%gdzie pierwszy w zakresie (0,1), drugi(-2,2) i jeden neuron.
+net=newp([0 1; -2 2],1); % tworzy perceptron składajacy sie z 2 elementow,
+% gdzie pierwszy w zakresie (0,1), drugi(-2,2) i jeden neuron.
 
 W=[0 0 1 1; 0 1 0 1];
 T=[0 0 0 1]; 
+% parametry
 
 net=init(net); 
-%inicjalizacja dowolnymi parametrami
+% inicjalizacja dowolnymi parametrami
 
 sym_przed=sim(net,W)
-%SiMULiNK - symulacja 
+% symulacja w SiMULiNK 
 % pierwszy argument to model
 % a drugi parametry
 
 net.trainParam.epochs=10; 
-%maximum epok to 20
+% maksymalna wartość iteracji to 20;
 net=train(net,W,T);
+% uczenie sieci
 sym_po = sim(net,W)
